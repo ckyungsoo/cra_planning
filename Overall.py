5c7366945086da76ac179621ef87d603df068728
ckyungsoo <110265866+ckyungsoo@users.noreply.github.com>
2023-01-20 12:32:09 +0900

    Update Overall.py

--- Overall.py
+++ Overall.py
@@ -8,6 +8,7 @@ rsk_assmnt = pd.read_csv('risk_assessment.csv', encoding = 'euc-kr')
 df = rsk_assmnt[rsk_assmnt['date']=='2022-09-30'].groupby(['engagement','LoB','rsk_idx_1'])['risk_index'].sum().reset_index()
 
 co11, co12 = st.columns(2)
+
 with col1:
   #본부별 인게이지먼트별 감리위험요소평가 식별 개수
   st.write('본부별 감리위험요소가 식별된 인게이지먼트 개수')
