7c33cdd641f4ed4546b519ca641eec9d0f6423c3
ckyungsoo <110265866+ckyungsoo@users.noreply.github.com>
2023-01-20 13:05:41 +0900

    Update Overall.py

--- Overall.py
+++ Overall.py
@@ -14,6 +14,6 @@ st.plotly_chart(fig_1, theme = "streamlit", use_contatiner_width = True)
 
 
 #본부별 인게이지먼트별 감사인감리대상 식별 개수
-st.subheader("blue[본부별 감사인감리대상위험이 식별된 인게이지먼트 개수]")
+st.subheader(':gray[본부별 감사인감리대상위험이 식별된 인게이지먼트 개수]')
 fig_2 = px.box(df[df['rsk_idx_1']=='2 감사인 감리 대상 개별감사업무 선정'] , x = 'LoB', y = 'risk_index')
 st.plotly_chart(fig_2, theme = "streamlit", use_contatiner_width = True)
