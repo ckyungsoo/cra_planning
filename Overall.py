b11a47e228de3b63127bada28a062cf849bac0fe
ckyungsoo <110265866+ckyungsoo@users.noreply.github.com>
2023-01-20 12:37:08 +0900

    Update Overall.py

--- Overall.py
+++ Overall.py
@@ -9,13 +9,13 @@ df = rsk_assmnt[rsk_assmnt['date']=='2022-09-30'].groupby(['engagement','LoB','r
 
 co11, co12 = st.columns(2)
 
-with col1:
+with co11:
   #본부별 인게이지먼트별 감리위험요소평가 식별 개수
   st.write('본부별 감리위험요소가 식별된 인게이지먼트 개수')
   fig_1 = px.box(df[df['rsk_idx_1']=='1 감리위험요소평가'] , x = 'LoB', y = 'risk_index')
   st.plotly_chart(fig_1, theme = "streamlit", use_contatiner_width = True)
 
-with col2:
+with co12:
   #본부별 인게이지먼트별 감사인감리대상 식별 개수
   st.write('본부별 감사인감리대상위험이 식별된 인게이지먼트 개수')
   fig_2 = px.box(df[df['rsk_idx_1']=='2 감사인 감리 대상 개별감사업무 선정'] , x = 'LoB', y = 'risk_index')
