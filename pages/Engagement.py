c76b71add33df0c8b50cf4a4612bfaf9c25e55a6
ckyungsoo <110265866+ckyungsoo@users.noreply.github.com>
2023-01-24 23:45:15 +0900

    Update Engagement.py

--- pages/Engagement.py
+++ pages/Engagement.py
@@ -3,3 +3,10 @@ import pandas as pd
 import os
 import numpy as np
 import plotly.express as px
+
+rsk_assmnt = pd.read_csv('risk_assessment.csv', encoding = 'euc-kr')
+
+with st.sidebar:
+    d = st.date_input("조회일자를 선택하세요", datetime.date(2022,9,30))
+    st.write('기준일: ', d.strftime('%Y년 %m월 %d일'))
+    eng = st.selectbox("Engagment를 입력하세요", list(set(rsk_assmnt['engagment'])))
