409c08e7897132d646b6a7a60eeb92d4ea2246b7
ckyungsoo <110265866+ckyungsoo@users.noreply.github.com>
2023-01-25 00:25:40 +0900

    Update Engagement.py

--- pages/Engagement.py
+++ pages/Engagement.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import datetime
 
 rsk_assmnt = pd.read_csv('risk_assessment.csv', encoding = 'euc-kr')
+fs = pd.read_csv('fs.csv',encoding = 'euc-kr')
 
 with st.sidebar:
     d = st.date_input("조회일자를 선택하세요", datetime.date(2022,9,30))
@@ -42,3 +43,22 @@ rmm = pd.read_excel('corp_industry.xlsx', sheet_name = 'rmm')
 industry_selected = corp_industry['Industry'][corp_industry['회사명']== eng].iloc[0]
 rmm_selected = rmm[rmm['Industry']==industry_selected]
 st.write(rmm_selected[['ClientID2','Description']])
+
+st.subheader('요약재무제표')
+st.write(eng,'의',d.strftime("%Y년%m월%d일"),'기준 재무현황은 다음과 같습니다.')
+tab_1, tab_2 = st.tabs(['연결','별도'])
+with tab_1 :
+    tab1, tab2= st.tabs(['재무상태표', '손익계산서'])
+    with tab1:
+        st.write(fs[['항목명','당기','전기']][(fs['연결/별도']=='연결')&(fs['재무제표구분']=='BS')&(fs['회사명']==eng)&(fs['결산기준일']==d.strftime("%Y-%m-%d"))])
+    with tab2:
+        st.write(fs[['항목명','당기','전기']][(fs['연결/별도']=='연결')&(fs['재무제표구분']=='PL')&(fs['회사명']==eng)&(fs['결산기준일']==d.strftime("%Y-%m-%d"))])
+
+with tab_2:
+    tab3, tab4= st.tabs(['재무상태표', '손익계산서'])
+    with tab3:
+        st.write(fs[['항목명','당기','전기']][(fs['연결/별도']=='별도')&(fs['재무제표구분']=='BS')&(fs['회사명']==eng)&(fs['결산기준일']==d.strftime("%Y-%m-%d"))])
+    with tab4:
+        st.write(fs[['항목명','당기','전기']][(fs['연결/별도']=='별도')&(fs['재무제표구분']=='PL')&(fs['회사명']==eng)&(fs['결산기준일']==d.strftime("%Y-%m-%d"))])
+
+
