d6320bc08e3c5c0d11619f410eefbbcf1ffeb86d
ckyungsoo <110265866+ckyungsoo@users.noreply.github.com>
2023-01-24 18:27:19 +0900

    Update Overall.py

--- Overall.py
+++ Overall.py
@@ -144,7 +144,11 @@ with col6:
 st.subheader('감리위험요소 항목별 세부')
 rsk = '1 감리위험요소평가'
 rsk_table_lob_1 = rsk_assmnt[(rsk_assmnt['date']==d.strftime('%Y-%m-%d'))&(rsk_assmnt['LoB']==lob_2)&(rsk_assmnt['rsk_idx_1']==rsk)].groupby(['engagement','rsk_idx_2'])['risk_index'].sum().unstack()
-st.table(rsk_table_lob_1)                                 
+st.table(rsk_table_lob_1)
+engmnt = st.selectbox('Engagement 선택',tuple(set(rsk_assmnt[(rsk_assmnt['date']==d.strftime('%Y-%m-%d'))&(rsk_assmnt['LoB']==lob_2)].engagement)))
+rsk_assmnt_sorted = rsk_assmnt[(rsk_assmnt['date']==d.strftime('%Y-%m-%d'))&(rsk_assmnt['LoB']==lob)&(rsk_assmnt['rsk_idx_1']==rsk)&(rsk_assmnt['engagement']==engmnt)&(rsk_assmnt['risk_index']>0)][['rsk_idx_2','rsk_idx_3']]
+rsk_assmnt_sorted.rename(columns={'rsk_idx_2':'구분','rsk_idx_3':'내용'})
+                      
 #감사인 감리대상
 st.subheader('감사인감리대상위험 항목별 세부')
 rsk = '2 감사인 감리 대상 개별감사업무 선정'
